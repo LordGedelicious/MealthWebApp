7162ec85dacb39f27139ad95fc12d4819e8afcc7
FelineJTD <16520335@std.stei.itb.ac.id>
2022-02-15 21:18:30 +0700

    add images, modify navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
-//import
+import Logo from "../images/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="absolute bg-transparent h-[3em] w-[100%] px-[2em] flex flex-row justify-between items-center text-bw-light">
+    <div className="absolute bg-transparent h-[5em] w-[100%] flex flex-row justify-between items-center text-bw-light bg-red">
       {/* Logo and Title */}
-      <div>
-        {/* Insert logo here */}
+      <div className="flex flex-row items-center">
+        <img src={Logo} alt="Logo" className="h-[5em]" />
         <h2>Mealth</h2>
       </div>
 
       {/* Links */}
-      <div className="flex flex-row justify-between w-[25em]">
+      <div className="flex flex-row justify-between w-[25em] pr-[3em]">
         <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
         <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
         <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
