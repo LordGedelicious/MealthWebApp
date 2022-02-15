117972bb94e6fa8e8f4003ba4a11d461ad26dec3
FelineJTD <16520335@std.stei.itb.ac.id>
2022-02-15 12:19:38 +0700

    add theme colours and fonts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div className="h-[100vh] w-[100%] p-[50vh]">
+      <div className="bg-white h-[100vh] w-[100%] p-[50vh]">
         <h1>Welcome to Mealth!</h1>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,22 @@ module.exports = {
       xl: '1440px',
     },
     colors: {
-    
+      black: '#303032',
+      white: '#F4F1EB',
+      green: '#E0E785',
+      orange: '#FABA8A',
+      red: '#DF826B',
+      blue: '#9FAED9',
+      spurple: '#C19AC7',
+      speach: '#FEB29B',
+      sorange: '#F6B05E',
+      syellow: '#FFDE78',
+      sgreen: '#AAE492',
+      sblue: '#B3CEF8',
     },
     fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Philosopher', 'serif'],
     },
     extend: {
       spacing: {
