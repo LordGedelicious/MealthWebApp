cda115f186fb68c2c1a6e4bbc8cd66a38046e896
FelineJTD <16520335@std.stei.itb.ac.id>
2022-02-20 22:21:04 +0700

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,26 @@ import Logo from "../images/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="absolute bg-transparent h-[5em] w-[100%] flex flex-row justify-between items-center text-bw-light bg-red">
+    <div className="absolute bg-[#FFFFFF] h-[5em] w-[100%] flex flex-row justify-between items-center text-black">
       {/* Logo and Title */}
       <div className="flex flex-row items-center">
         <img src={Logo} alt="Logo" className="h-[5em]" />
-        <h2>Mealth</h2>
+        <h3 className="text-2xl"><b>
+          <span className="text-red">m </span>
+          <span className="text-sorange">e </span>
+          <span className="text-syellow">a </span>
+          <span className="text-green">l </span>
+          <span className="text-blue">t </span>
+          <span className="text-spurple">h</span>
+        </b></h3>
       </div>
 
       {/* Links */}
-      <div className="flex flex-row justify-between w-[25em] pr-[3em]">
-        <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
-        <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
-        <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
-        <p className="hover:underline hover:underline-offset-8 hover:decoration-2 cursor-pointer hover:transition ease-in-out delay-150">AAA</p>
+      <div className="flex flex-row justify-between w-[30em] pr-[3em] md:visible sm:invisible">
+        <p className="w-[3.5em] hover:font-bold cursor-pointer hover:transition ease-in-out delay-150">Utama</p>
+        <p className="w-[3em] hover:font-bold cursor-pointer hover:transition ease-in-out delay-150">Acara</p>
+        <p className="w-[5.2em] hover:font-bold cursor-pointer hover:transition ease-in-out delay-150">Konseling</p>
+        <p className="w-[4.2em] hover:font-bold cursor-pointer hover:transition ease-in-out delay-150">Chatbot</p>
       </div>
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <div className="bg-white h-[100vh] w-[100%] p-[50vh]">
         <h1>Welcome to Mealth!</h1>
       </div>
+      <Footer />
     </>
   );
 };
